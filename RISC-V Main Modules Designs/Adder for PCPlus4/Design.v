66dd497f7588f870d8fa3a3862a76c583dc137a8
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-06-27 08:20:00 +0530

    Update Design.v

--- RISC-V Main Modules Designs/Adder for PCPlus4/Design.v
+++ RISC-V Main Modules Designs/Adder for PCPlus4/Design.v
@@ -1,22 +1,8 @@
 // Adder for result PCPlus4
 
-module Adder_for_PCPlus4_tb;
-reg pc_out;
-wire PCPlus4;
-
-Adder_for_PCPlus4 dut(pc_out, PCPlus4);
-
-initial begin 
-$dumpfile("dump.file");
-$dumpvars;
-
-pc_out<=1'b0;
-
-#10 $finish();
-end
-
-always #2 pc_out<=~pc_out;
-
+module Adder_for_PCPlus4(
+input pc_out,
+output PCPlus4
+    );
+    assign PCPlus4 = pc_out + 4; // Increment for next instruction in program counter by four
 endmodule
-
-
