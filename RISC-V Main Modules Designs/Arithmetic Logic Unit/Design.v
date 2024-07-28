5ab790b30f2e8e0e1c831b968a766e59e0fde84d
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2024-07-28 17:35:32 +0530

    Update Design.v

--- RISC-V Main Modules Designs/Arithmetic Logic Unit/Design.v
+++ RISC-V Main Modules Designs/Arithmetic Logic Unit/Design.v
@@ -1,24 +1,18 @@
-// ALU Control
-
-module ALUControl(
-input [6:0]op,
-input [1:0]ALUop,
-output reg [11:0]MainDecoder 
-    );
-   
-    wire PCSrc, ResultSrc, MemWrite, ALUSrc, ImmSrc, RegWrite, PCUpdate;
-    wire zero, Branch;
-    assign PCSrc = zero&Branch;
-    always@(*) begin
-    case({RegWrite, ImmSrc, ALUSrc, MemWrite, ResultSrc, Branch, ALUop, PCUpdate})
-    
-    7'b0000011 : MainDecoder <= 11'b1_00_1_0_1_0_00_0 ;// Load Word Instruction
-    7'b0100011 : MainDecoder <= 11'b0_01_1_1_x_0_00_0 ;// Store word Instruction
-    7'b0110011 : MainDecoder <= 11'b1_xx_0_0_0_0_10_0 ;//R Type Instruction
-    7'b1100011 : MainDecoder <= 11'b0_10_0_0_xx_1_01_0 ;// BEQ Instruction
-    7'b0010011 : MainDecoder <= 11'b1_00_1_0_0_0_10_0 ;// Add Immediate Instruction
-    7'b1101111 : MainDecoder <= 11'b1_11_x_0_10_0_xx_1 ;// JAL Instruction
-    default : MainDecoder <=11'bxxxxxxxxx;
-    endcase
-end    
+module ALU(
+    input [31:0] SrcA, SrcB,
+    input [2:0] ALUControl,
+    output reg zero,
+    output reg [31:0] ALUResult 
+);
+    always @(*) begin
+        case(ALUControl)
+            3'b000: ALUResult <= SrcA + SrcB; // Addition
+            3'b001: ALUResult <= SrcA - SrcB; // Subtraction
+            3'b010: ALUResult <= SrcA & SrcB; // AND operation
+            3'b011: ALUResult <= SrcA | SrcB; // OR operation
+            3'b100: ALUResult <= (SrcA < SrcB) ? 1 : 0 ; // SLT (set less than) operation
+            3'b101: zero <= (SrcA == SrcB); // BEQ (Branch if equal) operation
+            default: ALUResult <= 32'bx; // DONT CARE
+        endcase
+    end
 endmodule
